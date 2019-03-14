3aff922c434d1a9f3f053d7c6dce5fb4f90fb683
ferdy <xv.ferdy@gmail.com>
2019-03-15 06:52:39 +0700

    Add javascript

---
+++ script.js
@@ -0,0 +1,19 @@
+var tidak1 = document.querySelector('.tidak1');
+var tidak2 = document.querySelector('.tidak2');
+var tidak3 = document.querySelector('.tidak3');
+
+tidak1.addEventListener('mouseover', function () {
+    tidak1.classList.add('ilang');
+    tidak1.classList.remove('ada');
+    tidak2.classList.add('ada');
+})
+
+tidak2.addEventListener('mouseover', function () {
+    tidak2.classList.remove('ada');
+    tidak3.classList.add('ada');
+})
+
+tidak3.addEventListener('mouseover', function () {
+    tidak3.classList.remove('ada');
+    tidak1.classList.add('ada');
+})
