9fce357491ca5b4a2d87a11c507923b35d8b3723
ferdy <xv.ferdy@gmail.com>
2019-03-15 08:07:08 +0700

    Add click event

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var tidak1 = document.querySelector('.tidak1');
 var tidak2 = document.querySelector('.tidak2');
 var tidak3 = document.querySelector('.tidak3');
 
+//mouse hover
 tidak1.addEventListener('mouseover', function () {
     tidak1.classList.add('ilang');
     tidak1.classList.remove('ada');
@@ -16,4 +17,17 @@ tidak2.addEventListener('mouseover', function () {
 tidak3.addEventListener('mouseover', function () {
     tidak3.classList.remove('ada');
     tidak1.classList.add('ada');
+})
+
+//mouse click
+tidak1.addEventListener('click', function () {
+    alert('Tombol sedang dalam maintenance');
+})
+
+tidak2.addEventListener('click', function () {
+    alert('Tombol sedang dalam maintenance');
+})
+
+tidak3.addEventListener('click', function () {
+    alert('Tombol sedang dalam maintenance');
 })
